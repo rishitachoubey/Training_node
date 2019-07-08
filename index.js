2b9d14391d28e0dba32b62862a55da67776bfda0
Rishita Choubey <46817010+rishitachoubey@users.noreply.github.com>
2019-07-09 00:28:42 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,96 +1,88 @@
-const express= require('express');
-const app= express();
+const express = require('express');
+const app = express();
 app.use(express.json());
-const Joi= require('joi');
-const courses= [
-{id:1, name:'Rishita'},
-{id:2, name:'Riya'},
-{id:3, name:'Anirudh'},
-{id:4, name:'Tanu'}
+const Joi = require('joi');
+const courses = [
+    { id: 1, name: 'Rishita' },
+    { id: 2, name: 'Riya' },
+    { id: 3, name: 'Anirudh' },
+    { id: 4, name: 'Tanu' }
 
 ];
 
 
 //to get all the courses
 
-app.get('/api/courses/', (req,res)=>
+app.get('/api/courses/', (req, res) => {
 
-{
-
-res.send(courses);
+    res.send(courses);
 
 
 });
 
 // to get course of specified id
 
-app.get('/api/courses/:id', (req,res)=>
+app.get('/api/courses/:id', (req, res) => {
 
-{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
 
-const course= courses.find(c=> c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Error!! course not found!');
 
-if(!course)res.status(404).send('Error!! course not found!');
-
-res.send(course);
+    res.send(course);
 
 
 });
 
 //Handling Post requests
 
-app.post('/api/courses', (req,res)=>
+app.post('/api/courses', (req, res) => {
 
-{
+    const { error } = validateCourse(req.body);
 
-const{error}= validateCourse(req.body);
+    if (error) {
 
-if(error){
+        res.status(400).send(result.error);
+        return;
 
-res.status(400).send(result.error);
-return;
-
-}
-
-const course= {
-
-id: courses.length+1,
-name: req.body.name
-
-};
-
-courses.push(course);
-res.send(course);
-
-
-});
-
-app.put('/api/courses/:id', (req,res)=>
-
-{
-
-    const course= courses.find(c=> c.id === parseInt(req.params.id));
-
-if(!course)res.status(404).send('Error!! course not found!');
-
-const{error}= validateCourse(req.body);
-if(error){
-
-    res.status(400).send(error.details[0].message);
-    return;
-    
-    
     }
-    
 
-course.name= req.body.name;
-res.send(course);
+    const course = {
+
+        id: courses.length + 1,
+        name: req.body.name
+
+    };
+
+    courses.push(course);
+    res.send(course);
+
+
 });
 
-function validateCourse(course){
+app.put('/api/courses/:id', (req, res) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (!course) res.status(404).send('Error!! course not found!');
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+
+        res.status(400).send(error.details[0].message);
+        return;
 
 
-    const schema= {
+    }
+
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+
+
+    const schema = {
 
         name: Joi.string().min(3).required()
 
@@ -98,21 +90,19 @@ function validateCourse(course){
 
 
 
-return Joi.validate(course, schema);
+    return Joi.validate(course, schema);
 }
 
-app.delete('/api/courses/:id', (req,res)=>
+app.delete('/api/courses/:id', (req, res) => {
 
-{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
 
-const course= courses.find(c=> c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Error!! course not found!');
 
-if(!course)res.status(404).send('Error!! course not found!');
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
 
-const index= courses.indexOf(course);
-courses.splice(index, 1);
-
-res.send(course);
+    res.send(course);
 
 
 });
@@ -120,4 +110,4 @@ res.send(course);
 
 
 
-app.listen(3000,()=> console.log('Listening to port 3000'))
+app.listen(3000, () => console.log('Listening to port 3000'))
